9a592bca426e964c5b1215ddf3416255918bcc12
saudahabib <saudababs00@gmail.com>
2019-02-15 15:17:03 +0300

    Create variables to hold our input

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ function calculate()	{
 var side1a= document.getElementById("first").value;
 var side2a= document.getElementById("second").value;
 var side3a= document.getElementById("third").value;
+var side1=parseInt(side1a);
+var side2=parseInt(side2a);
+var side3=parseInt(side3a);
