5e423f55e9e832386bcad12f5080e33312f1e0ce
saudahabib <saudababs00@gmail.com>
2019-02-15 15:20:41 +0300

    start working on coditions in JS

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ var side1=parseInt(side1a);
 var side2=parseInt(side2a);
 var side3=parseInt(side3a);
 //This is my business logic//
+
+var values=[side1,side2,side3];
+if(values[0]===values[1] && values[1]===values[2] && values[2]===values[0])	{
+ 	alert ("The triangle is equilateral");
+ }
+else if (values[0]===values[1] || values[1]===values[2] || values[0]===values[2])	{
+ 	alert("The triangle is isoceles")
+ }
+ else if(side1+side2<=side3 || side1+side3<=side2 || side2+side3<=side1)	{
+	alert("These values do not make a triangle")
+}
+
+else if(side1!==side2 && side1!==side3 && side2!==side1 && side2!==side3 && side3!==side2 && side3!==side1 && side1+side2>side3 || side2+side3>side1 || side3+side1> side2 )	{
+	alert("The triangle is scalene")
+}
+else{
+	alert("Please enter a numeric value")
+}
