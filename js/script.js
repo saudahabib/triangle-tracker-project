adf416d24855f5c5a1a86ca51bcf625160f96d19
saudahabib <saudababs00@gmail.com>
2019-02-15 15:15:41 +0300

    Add a README file

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function calculate()	{
+var side1a= document.getElementById("first").value;
+var side2a= document.getElementById("second").value;
+var side3a= document.getElementById("third").value;
