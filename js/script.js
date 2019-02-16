8d3e0b22d8d4d7d44a200720a8de40dc4cc67487
saudahabib <saudababs00@gmail.com>
2019-02-16 11:58:13 +0300

    change alerts to inner html

--- js/script.js
+++ js/script.js
@@ -15,17 +15,17 @@ if(values[0]===values[1] && values[1]===values[2] && values[2]===values[0])	{
  	document.getElementById('show').innerHTML= 'The triangle is equilateral <br> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKR-4u9PRtlGT3q6wugn10dzOFAowxTeu2waTzKfgwfeH1562eCA">'
  }
 else if (values[0]===values[1] || values[1]===values[2] || values[0]===values[2])	{
- 	alert("The triangle is isoceles")
+document.getElementById('show').innerHTML='The triangle is isoceles <br> <img src="http://2000clicks.com/MathHelp/IsoscelesTriangleXHB.gif">'
  }
  else if(side1+side2<=side3 || side1+side3<=side2 || side2+side3<=side1)	{
-	alert("These values do not make a triangle")
+document.getElementById('show').innerHTML='These values do not make a triangle <br> <img src="https://media.tenor.com/images/020e1899e91ed2742fd0491927d90401/tenor.gif">'
 }
 
 else if(side1!==side2 && side1!==side3 && side2!==side1 && side2!==side3 && side3!==side2 && side3!==side1 && side1+side2>side3 || side2+side3>side1 || side3+side1> side2 )	{
-	alert("The triangle is scalene")
+	document.getElementById('show').innerHTML='The triangle is scalene <br> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHXUOrQnZKiso7SF_amW37JV4548WCDB8ah5GZdAhK2iJO6X2XRQ">'
 }
 else {
-	alert("Please enter a numeric value")
+ document.getElementById('show').innerHTML='Please enter a numeric value <mg src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZDwLKpK4jt17Vprx6ONFMqpc3DlfOxVIIt_kSZm9ySPSoJBmb">'
 }
 
 }
