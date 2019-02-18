00c25a5cec3b08b95105729b58dfceb7c60a11fe
saudahabib <saudababs00@gmail.com>
2019-02-18 10:36:34 +0300

    Correct any grammatical errors

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ else if (values[0]===values[1] || values[1]===values[2] || values[0]===values[2]
 else if(side1+side2<=side3 || side1+side3<=side2 || side2+side3<=side1)	{
  document.getElementById('show').innerHTML='These values do not make a triangle <br> <img src="https://media.tenor.com/images/020e1899e91ed2742fd0491927d90401/tenor.gif">'
 }
-else if(side1!==side2 && side1!==side3 && side2!==side1 && side2!==side3 && side3!==side2 && side3!==side1 && side1+side2>side3 || side2+side3>side1 || side3+side1> side2 )	{
+else if(side1!==side2 && side1!==side3 && side2!==side1 && side2!==side3 && side3!==side2 && side3!==side1 &&           side1+side2>side3 || side2+side3>side1 || side3+side1> side2 )	{
 	document.getElementById('show').innerHTML='The triangle is scalene <br> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHXUOrQnZKiso7SF_amW37JV4548WCDB8ah5GZdAhK2iJO6X2XRQ">'
 }
 else {
